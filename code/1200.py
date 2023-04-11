db082853ce4d8e295af08e6105a414d078f23b3d
franciscasaldivar <126597699+franciscasaldivar@users.noreply.github.com>
2023-04-11 15:40:40 -0400

    Add files via upload

---
+++ code/1200.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+
+# Read the TSV file into a DataFrame
+df = pd.read_csv('/Users/franciscasaldivar/montesinos/data/modified_data/modified_data/1200.tsv', sep='\t')
+
+# Perform modifications on the DataFrame
+#list of strings for matching
+people = ["MONTESINOS", "CROUSILLAT"]
+
+# Function to get the best match from the given list
+def get_best_match(query, choices):
+    best_match = process.extractOne(query, choices)
+    return best_match[0]
+
+# Apply fuzzy matching to match_list for each row in source_column
+df['speaker_std'] = df['speaker'].apply(lambda x: get_best_match(x, people))
+
+# Write the updated DataFrame back to a TSV file
+df.to_csv('/Users/franciscasaldivar/montesinos/data/modified_data/modified_data/1200.tsv', sep='\t', index=False)
+
